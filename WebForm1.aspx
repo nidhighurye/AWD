7cfab5a34b6806f08d5e66e3bdd6dfe376ae6089
nidhighurye <167406697+nidhighurye@users.noreply.github.com>
2024-10-20 12:00:53 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication16.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="z-index: 1; left: 161px; top: 228px; position: absolute" Text="Disconnected" />
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="connected" />
+        </p>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" ProviderName="<%$ ConnectionStrings:ConnectionString.ProviderName %>" SelectCommand="SELECT * FROM [Emp]"></asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication16
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Getdb()
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("Select * from Emp", conn);
+        
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            ad.Fill(ds,"Emp");
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+          
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            Getdb();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn=new SqlConnection(ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("Select * from Emp",conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            GridView1.DataSource = reader;
+            GridView1.DataBind();
+            conn.Close();
+
+        }
+    }
+}
